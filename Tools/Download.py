445c7f24344779358826cabf7e09ab3d4bfd6d29
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2021-03-10 04:39:48 +0100

    Update Download.py

--- Tools/Download.py
+++ Tools/Download.py
@@ -94,4 +94,34 @@ async def download(c, m):
             img.save(thumb_image_path, "JPEG")
         c_time = time.time()
 
-        await upload_video(c, m, send, media_location, thumb_image_path, duration, width, height, cp)
+            await send.edit(Translation.UPLOAD_START)
+    c_time = time.time()
+    if m.video:
+        await c.send_video(
+            chat_id=m.chat.id,
+            video=media_location,
+            caption=f"{cp} | @MoviesBdarija",
+            duration=duration,
+            width=width,
+            height=height,
+            supports_streaming=True,
+            thumb=thumb_image_path,
+            reply_to_message_id=m.message_id,
+            progress=progress_for_pyrogram,
+            progress_args=("Upload Status:", send, c_time),
+        )
+    if m.text == "/converttofile":
+        await c.send_document(
+            chat_id=m.chat.id,
+            document=media_location,
+            thumb=thumb_image_path,
+            reply_to_message_id=m.reply_to_message.message_id,
+            progress=progress_for_pyrogram,
+            progress_args=("Upload Status:", send, c_time),
+        )
+    try:
+        os.remove(media_location)
+    except BaseException:
+        pass
+    await send.edit(Translation.UPLOAD_COMPLETE)
+    logger.info(f"{m.from_user.first_name}'s Task completed")
