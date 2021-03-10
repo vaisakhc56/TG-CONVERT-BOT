f62bc0f872488956ef6e2b8f3d4cc08c5436a2c7
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2021-03-10 01:18:14 +0100

    Update Download.py

--- Tools/Download.py
+++ Tools/Download.py
@@ -48,8 +48,6 @@ async def download(c, m):
         height = 0
         duration = 0
         metadata = extractMetadata(createParser(media_location))
-            if metadata.has("duration"):
-                duration = metadata.get('duration').seconds
         thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(m.from_user.id) + ".jpg"
 
             if not os.path.exists(thumb_image_path):
@@ -73,6 +71,8 @@ async def download(c, m):
             logger.info(thumb_image_path)
 
             metadata = extractMetadata(createParser(thumb_image_path))
+            if metadata.has("duration"):
+                duration = metadata.get('duration').seconds
             if metadata.has("width"):
                 width = metadata.get("width")
             if metadata.has("height"):
