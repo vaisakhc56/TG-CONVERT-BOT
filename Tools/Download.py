e858fcaa8806b78af99c782b89bd7ecb490ff284
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2021-03-10 03:07:01 +0000

    DAMIEN : ⚙ Fixes

--- Tools/Download.py
+++ Tools/Download.py
@@ -1,27 +1,23 @@
+from translation import Translation
+from Tools.upload import upload_video
+from Tools.screenshot import take_screen_shot
+from Tools.progress import progress_for_pyrogram
+from database.database import *
+from config import Config
+from pyrogram import Client, filters
+from PIL import Image
+from hachoir.parser import createParser
+from hachoir.metadata import extractMetadata
+import re
+import time
+import random
+import os
 import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-
-import os
-import random
-import time
-import re
-
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
-from PIL import Image
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-from config import Config
-from database.database import *
-from Tools.progress import progress_for_pyrogram
-from Tools.screenshot import take_screen_shot
-from Tools.upload import upload_video
-from translation import Translation
 
 
 # Download the media
