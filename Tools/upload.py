d4c9a45f391b3287e508dcdf6744aabeb0396ef6
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2021-03-10 02:36:40 +0100

    Update upload.py

--- Tools/upload.py
+++ Tools/upload.py
@@ -15,15 +15,6 @@ logger = logging.getLogger(__name__)
 async def upload_video(
     c, m, send, media_location, thumb_image_path, duration, width, height
 ):
-            if m.caption != None:
-                try:
-                    txt = update.caption
-                    cp = re.sub("@\\S+", "", txt)
-                except:
-                    pass
-            if m.caption == None:
-                cp = m.video.file_name
-
     await send.edit(Translation.UPLOAD_START)
     c_time = time.time()
     if m.video:
@@ -37,15 +28,6 @@ async def upload_video(
             supports_streaming=True,
             thumb=thumb_image_path,
             reply_to_message_id=m.message_id,
-            progress=progress_for_pyrogram,
-            progress_args=("Upload Status:", send, c_time),
-        )
-    if m.document:
-        await c.send_document(
-            chat_id=m.chat.id,
-            document=media_location,
-            thumb=thumb_image_path,
-            reply_to_message_id=m.reply_to_message.message_id,
             progress=progress_for_pyrogram,
             progress_args=("Upload Status:", send, c_time),
         )
