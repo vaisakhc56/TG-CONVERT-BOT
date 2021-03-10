d4045d7fa9caf1f749d8de836f72b5150596b5c4
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2021-03-10 02:54:24 +0100

    Update upload.py

--- Tools/upload.py
+++ Tools/upload.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import re
 import time
 
 from Tools.progress import progress_for_pyrogram
@@ -14,20 +15,37 @@ logger = logging.getLogger(__name__)
 async def upload_video(
     c, m, send, media_location, thumb_image_path, duration, width, height
 ):
+            if m.caption != None:
+                try:
+                    txt = m.caption
+                    cp = re.sub("@\S+", "", txt)
+                except:
+                    pass
+            if m.caption == None:
+                cp = m.video.file_name
+
     await send.edit(Translation.UPLOAD_START)
     c_time = time.time()
     if m.video:
         await c.send_video(
             chat_id=m.chat.id,
             video=media_location,
-            #            caption=f"{cp} | @MoviesBdarija",
-            caption=m.caption,
+            caption=f"{cp} | @MoviesBdarija",
             duration=duration,
             width=width,
             height=height,
             supports_streaming=True,
             thumb=thumb_image_path,
             reply_to_message_id=m.message_id,
+            progress=progress_for_pyrogram,
+            progress_args=("Upload Status:", send, c_time),
+        )
+    if m.text == "/converttofile":
+        await c.send_document(
+            chat_id=m.chat.id,
+            document=media_location,
+            thumb=thumb_image_path,
+            reply_to_message_id=m.reply_to_message.message_id,
             progress=progress_for_pyrogram,
             progress_args=("Upload Status:", send, c_time),
         )
