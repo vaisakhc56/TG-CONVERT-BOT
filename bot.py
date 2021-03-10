9bf2871f3c16fbd2e16e7f19e4ac8c5a340d9376
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2021-03-10 02:51:53 +0000

    DAMIEN : ⚙ Fixes

--- bot.py
+++ bot.py
@@ -1,22 +1,20 @@
 import os
 
-from config import Config
-
 from pyrogram import Client
 
-if __name__ == "__main__" :
+from config import Config
+
+if __name__ == "__main__":
     # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
-    plugins = dict(
-        root="plugins"
-    )
+    plugins = dict(root="plugins")
     app = Client(
         "C2V",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
-        plugins=plugins
+        plugins=plugins,
     )
     Config.AUTH_USERS.add(853393439)
     app.run()
