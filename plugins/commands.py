c2b702d6b53becf2d6fb02f0f5c7153a991583ef
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2021-03-10 03:54:29 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,34 +1,19 @@
-import logging
-
-from pyrogram import Client, Filters, InlineKeyboardButton, InlineKeyboardMarkup
+import os
 
 from config import Config
+
 from translation import Translation
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger = logging.getLogger(__name__)
+from pyrogram import Client, filters
 
 
-my_father = "https://t.me/{}".format(Config.USER_NAME[1:])
-support = "https://telegram.dog/Ns_Bot_supporters"
 
-
-@Client.on_message(Filters.command(["start"]))
+@Client.on_message(filters.private & filters.command(["start"]))
 async def start(c, m):
-
     await c.send_message(
         chat_id=m.chat.id,
         text=Translation.START.format(m.from_user.first_name, Config.USER_NAME),
-        reply_to_message_id=m.message_id,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("My Father 👨‍💻", url=my_father),
-                    InlineKeyboardButton("📌Support channel", url=support),
-                ]
-            ]
-        ),
+        reply_to_message_id=m.message_id
     )
-    logger.info(f"{m.from_user.first_name} used start command")
+
+
