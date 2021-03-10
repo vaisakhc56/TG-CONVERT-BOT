9f0342c1d875d552774b7c3967e238a9c6aab80a
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2021-03-10 03:51:21 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,22 @@
-import logging
 import os
-
-from pyrogram import Client
 
 from config import Config
 
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logger = logging.getLogger(__name__)
+from pyrogram import Client
 
-
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-
-if __name__ == "__main__":
+if __name__ == "__main__" :
     # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
-    plugins = dict(root="plugins")
+    plugins = dict(
+        root="plugins"
+    )
     app = Client(
-        "CONVERTBOT",
+        "C2V",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
-        plugins=plugins,
+        plugins=plugins
     )
-    Config.AUTH_USERS.add(1337144652)
+    Config.AUTH_USERS.add(853393439)
     app.run()
