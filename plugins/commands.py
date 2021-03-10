b08cae3840a845e6b5908b3a3b9f4e3b2493afe2
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2021-03-10 02:54:54 +0000

    DAMIEN : ⚙ Fixes

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,11 +1,9 @@
-import os
 
 from config import Config
 
 from translation import Translation
 
 from pyrogram import Client, filters
-
 
 
 @Client.on_message(filters.private & filters.command(["start"]))
@@ -15,5 +13,3 @@ async def start(c, m):
         text=Translation.START.format(m.from_user.first_name, Config.USER_NAME),
         reply_to_message_id=m.message_id
     )
-
-
