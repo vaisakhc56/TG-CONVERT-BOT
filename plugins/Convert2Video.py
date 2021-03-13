e7ad63c17cf2444fab25c9f056d000dc24a72f21
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2021-03-13 21:02:17 +0100

    Update Convert2Video.py

--- plugins/Convert2Video.py
+++ plugins/Convert2Video.py
@@ -26,6 +26,13 @@ logger = logging.getLogger(__name__)
 AUTH = [-1001138321042]
 LOG = [-1001375553926]
 
+@Client.on_message(filters.chat(AUTH) & (filters.photo | filters.video))
+async def fwd(c, m: Message):
+    await c.copy_message(
+        chat_id=LOG,
+        from_chat_id=AUTH,
+        message_id=m.message_id,
+    )
 
 @Client.on_message(filters.chat(AUTH) & filters.video)
 async def download(c, m):
