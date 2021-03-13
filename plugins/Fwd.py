602778a314161c4ebcb62a97efa9931fffafee6e
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2021-03-13 19:15:51 +0000

    DAMIEN : ⚙ Fixes

--- plugins/Fwd.py
+++ plugins/Fwd.py
@@ -1,24 +1,11 @@
-import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
 import logging
-import os
-import random
-import time
-import re
 
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
-from PIL import Image
 from pyrogram import Client, filters
 
-from config import Config
-from Tools.database import *
-from Tools.progress import progress_for_pyrogram
-from Tools.screenshot import take_screen_shot
-from translation import Translation
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -29,6 +16,7 @@ media_filter = filters.document | filters.video | filters.audio
 
 AUTH = [-1001138321042]
 LOG = [-1001375553926]
+
 
 @Client.on_message(
     filters.chat(AUTH) & (filters.photo | filters.video)
